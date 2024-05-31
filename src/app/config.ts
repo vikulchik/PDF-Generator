16dd5d073ed38cc8222fe141a0e067a9039b71d0
vikulchik <kvvendetta@gmail.com>
2024-05-31 16:38:20 +0200

    deleted docs folder

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,3 @@
 const API_KEY = "78684310-850d-427a-8432-4a6487f6dbc4";
 export const API_URL = `http://95.217.134.12:4010/create-pdf?apiKey=${API_KEY}`;
-
 export const PDF_HISTORY_STORAGE_KEY = 'pdf-history';
